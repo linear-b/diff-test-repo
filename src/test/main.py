5fabe2617fa0c3f3bbd8ac68dac72bd80d49d627
Roy Reshef <reshef.roy@gmail.com>
2020-03-30 16:15:31 +0300

    added print before calculation and deleted print after

--- src/test/main.py
+++ src/test/main.py
@@ -3,5 +3,5 @@ from src.test import simple_calculator
 if __name__ == '__main__':
     a = 1
     b = 2
+    print(f'calculating {a} plus {b}')
     res = simple_calculator.add(a, b)
-    print(f'{a} plus {b} is {res}')
